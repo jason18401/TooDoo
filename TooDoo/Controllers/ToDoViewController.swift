98f94a1820d7638ea77a92702ba58418330d9501
Jason <jason.yu92@live.com>
2019-05-13 23:57:01 -0400

    Turn to MVC model, and tableView cells checkmark working

--- TooDoo/Controllers/ToDoViewController.swift
+++ TooDoo/Controllers/ToDoViewController.swift
@@ -10,7 +10,8 @@ import UIKit
 
 class ToDoViewController: UITableViewController {
 
-    var itemArray = ["Find Food", "Buy Food", "get Food"]
+    //var itemArray = ["Find Food", "Buy Food", "get Food"]
+    var itemArray = [Item]()
     
     let defaults = UserDefaults.standard
     
@@ -18,10 +19,22 @@ class ToDoViewController: UITableViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
+        let newItem = Item()
+        newItem.title = "Find Mike"
+        itemArray.append(newItem)
+        
+        let newItem2 = Item()
+        newItem2.title = "Find Nemo"
+        itemArray.append(newItem2)
+        
+        let newItem3 = Item()
+        newItem3.title = "Find Dory"
+        itemArray.append(newItem3)
+        
         //set array as the updated array
-        if let items = defaults.array(forKey: "TodoListArray") as? [String]{
-            itemArray = items
-        }
+        //if let items = defaults.array(forKey: "TodoListArray") as? [String]{
+        //    itemArray = items
+        //}
     }
 
     //Mark - Tableview Datasource Methods
@@ -29,11 +42,18 @@ class ToDoViewController: UITableViewController {
         return itemArray.count
     }
 
+    //called initially when the tableView is loaded up
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
         
-        cell.textLabel?.text = itemArray[indexPath.row]
+        cell.textLabel?.text = itemArray[indexPath.row].title
+        
+        if itemArray[indexPath.row].done == true{
+            cell.accessoryType = .checkmark
+        }else{
+            cell.accessoryType = .none
+        }
         
         return cell
     }
@@ -44,14 +64,16 @@ class ToDoViewController: UITableViewController {
         //print item into debug console
         print(itemArray[indexPath.row])
         
-        //to add a checkmark
-        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark{
-            //to deselect checkmark if it is already checked
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        if itemArray[indexPath.row].done == false{
+            itemArray[indexPath.row].done = true
         }else{
-            //add checkmark if not checked
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+            itemArray[indexPath.row].done = false
         }
+        
+        //does the same thing as the above if-else. Does the opposite of what is set
+        //itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+        
+        tableView.reloadData()
         
         //turns the highlighted row back to the original color
         tableView.deselectRow(at: indexPath, animated: true)
@@ -66,7 +88,10 @@ class ToDoViewController: UITableViewController {
         
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
             
-            self.itemArray.append(textField.text!)
+            let newItem = Item()
+            newItem.title = textField.text!
+            
+            self.itemArray.append(newItem)
             
             self.defaults.set(self.itemArray, forKey: "TodoListArray")
             
---
+++ TooDoo/Data Model/Item.swift
@@ -0,0 +1,16 @@
+//
+//  Item.swift
+//  TooDoo
+//
+//  Created by Jason Yu on 5/13/19.
+//  Copyright © 2019 Jason Yu. All rights reserved.
+//
+
+import Foundation
+
+class Item{
+    var title : String = ""
+    var done : Bool = false
+    
+    
+}
