c1d7f74e34f350c74a73a81a07b20ba4364fb83a
Jason <jason.yu92@live.com>
2019-05-23 00:11:35 -0400

    Added category section

---
+++ TooDoo/Controllers/CategoryViewController.swift
@@ -0,0 +1,96 @@
+//
+//  CategoryTableViewController.swift
+//  TooDoo
+//
+//  Created by Jason Yu on 5/20/19.
+//  Copyright © 2019 Jason Yu. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class CategoryViewController: UITableViewController {
+
+    var categoryArray = [Category]()
+    
+    //singleton to access AppDelegate as Object
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        loadItems()
+    }
+    
+    // MARK: - Table view data source
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return categoryArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell", for: indexPath)
+        
+        cell.textLabel?.text = categoryArray[indexPath.row].name
+        
+        return cell
+    }
+
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Add New Category", message: "", preferredStyle: .alert)
+        
+        //clicks inside the alert
+        let action = UIAlertAction(title: "Add", style: .default) { (action) in
+            
+            let newCategory = Category(context: self.context)
+            newCategory.name = textField.text!
+            
+            self.categoryArray.append(newCategory)
+            
+            self.saveData()
+        }
+        
+        alert.addTextField { (field) in
+            field.placeholder = "Add a new category"
+            textField = field
+        }
+        
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
+    //MARK: Data Manipulation Methods - save and load data
+    func saveData(){
+        
+        do{
+            try context.save() //try to commit to our context to persistent container
+        }catch{
+            print("Error in saving Data \(error)")
+        }
+        
+        self.tableView.reloadData()
+    }
+    
+    func loadItems(){
+        
+        //need to read data from our context
+        let request : NSFetchRequest<Category> = Category.fetchRequest() //get back all NSFetchRequest Object that uses Category entity
+        
+        do{
+            categoryArray = try context.fetch(request)
+        }catch{
+            print("Error loading categories \(error)")
+        }
+        
+        tableView.reloadData()
+    }
+    
+    //MARK: TableView Delegate Methods
+    //leave
+    
+}
--- TooDoo/Controllers/ToDoViewController.swift
+++ TooDoo/Controllers/ToDoViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CoreData
 
 class ToDoViewController: UITableViewController {
 
@@ -14,14 +15,23 @@ class ToDoViewController: UITableViewController {
     var itemArray = [Item]()
     
     //filepath for persisting data
-    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
+    //let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
+    
+    //singleton to access AppDelegate as Object
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
+    @IBOutlet weak var searchBar: UISearchBar!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
+        //to find file location for CoreData
+        print(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask))
+        
        // print(dataFilePath)
         
+        searchBar.delegate = self
         
         //set array as the updated array
         //if let items = defaults.array(forKey: "TodoListArray") as? [String]{
@@ -58,6 +68,10 @@ class ToDoViewController: UITableViewController {
         //print item into debug console
         print(itemArray[indexPath.row])
         
+        //the order matters with these two statements
+        //context.delete(itemArray[indexPath.row])
+        //itemArray.remove(at: itemArray[indexPath.row])
+        
         if itemArray[indexPath.row].done == false{
             itemArray[indexPath.row].done = true
         }else{
@@ -67,6 +81,7 @@ class ToDoViewController: UITableViewController {
         //does the same thing as the above if-else. Does the opposite of what is set
         //itemArray[indexPath.row].done = !itemArray[indexPath.row].done
         
+        //commits the current state of the context
         saveItems()
         
         //turns the highlighted row back to the original color
@@ -82,9 +97,9 @@ class ToDoViewController: UITableViewController {
         
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
             
-            let newItem = Item()
-            newItem.title = textField.text!
-            
+            let newItem = Item(context: self.context) //Item Object comes from DataModel, type - NSManagedObject
+            newItem.title = textField.text! //1 of 2 attributes
+            newItem.done = false    //2 of 2 attributes
             self.itemArray.append(newItem)
             
             self.saveItems()
@@ -105,25 +120,59 @@ class ToDoViewController: UITableViewController {
     }
     
     func saveItems(){
-        let encoder = PropertyListEncoder()
         
         do{
-            let data = try encoder.encode(itemArray)
-            try data.write(to: dataFilePath!)
+            try context.save()
         }catch{
-            print(error)
+            print("Error saving context: \(error)")
         }
         
         self.tableView.reloadData()
     }
     
-    func loadItems(){
-        if let data = try? Data(contentsOf: dataFilePath!){
-            let decoder = PropertyListDecoder()
-            do{
-                itemArray = try decoder.decode([Item].self, from: data)
-            }catch{
-                print(error)
+    func loadItems(with request : NSFetchRequest<Item> = Item.fetchRequest()){ //Item.fetch... default value so you can call without passing it a value
+        let request : NSFetchRequest<Item> = Item.fetchRequest()
+        
+        do{
+            //load the results into the itemArray
+            itemArray = try context.fetch(request)
+        }catch{
+            print("Error fetching data from context \(error)")
+        }
+    }
+    
+}
+
+extension ToDoViewController: UISearchBarDelegate{
+    
+    func searchBarBookmarkButtonClicked(_ searchBar: UISearchBar) {
+        let request : NSFetchRequest<Item> = Item.fetchRequest()
+     
+        let predicate = NSPredicate(format: "title CONTAINS[cd] %@", searchBar.text!)
+        request.predicate = predicate
+        
+        let sortDescriptor = NSSortDescriptor(key: "title", ascending: true)
+        request.sortDescriptors = [sortDescriptor]
+        
+//        do{
+//            //load the results into the itemArray
+//            itemArray = try context.fetch(request)
+//        }catch{
+//            print("Error fetching data from context \(error)")
+//        }
+        
+        //does same thing as above do-catch statement
+        loadItems(with: request)
+        
+    }
+    
+    //when user presses the x the list gets reloaded
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchBar.text?.count == 0{
+            loadItems()
+            
+            DispatchQueue.main.async { //gets the code running in the background
+                searchBar.resignFirstResponder()//no longer currently selected, to get the blinking cursor and keyboard to go away
             }
         }
     }
